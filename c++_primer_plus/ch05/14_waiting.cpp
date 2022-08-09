9dd3b75a4e3da99f562cae3d99d06c543fed69ae
眰恦 <840194634@qq.com>
2022-08-09 10:51:24 +0800

    学习

--- c++_primer_plus/ch05/14_waiting.cpp
+++ c++_primer_plus/ch05/14_waiting.cpp
@@ -5,10 +5,19 @@
 #include <iostream>
 #include <ctime>
 
-int main(){
+int main() {
     using namespace std;
-    cout << "Enter the delay time, in seconds";
+    int i;
+    cout << "Enter the delay time, in seconds: ";
     float secs;
-
+    cin >> secs;
+    clock_t delay = secs * CLOCKS_PER_SEC;  // 转换为时钟刻度
+    cout << "starting\a\n";
+    clock_t start = clock();    // 等待到时间过去
+    while (clock() - start < delay) {
+        cout << "i = " << i << endl;
+        i++;
+    }
+    cout << "done \a\n";
     return 0;
 }
