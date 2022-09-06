9b626aeb155af6c6a3f0009f5b4f0fd18834bb7f
眰恦 <840194634@qq.com>
2022-09-06 15:17:07 +0800

    学习

---
+++ c++_primer_plus/ch06/2_ifelse.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char ch;
+	cout << "Type, and I shall repeat.\n";
+	cin.get(ch);
+	while (ch != '.')
+	{
+		if(ch == '\n')
+			cout << ch;
+		else
+			cout << ++ch;
+		cin.get(ch);
+	}
+	cout << "\nPlease excuse the slight confusion.\n";
+	return 0;
+
+}
