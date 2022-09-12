c378de4f971f6dc127cc556216347f29e639852c
眰恦 <840194634@qq.com>
2022-09-12 16:51:45 +0800

    学习

--- c++_primer_plus/ch06/15_outfile.cpp
+++ c++_primer_plus/ch06/15_outfile.cpp
@@ -2,8 +2,9 @@
 // Created by 84019 on 2022/9/12.
 //
 
-#include <iostream>
-#include <fstream>
+#include <iostream>     // ostream->cout    istream->cin
+#include <fstream>      // ofstream         ifstream
+//1.声明fstream
 
 using namespace std;
 
@@ -14,7 +15,8 @@ int main()
     double a_price; // 生产时的价格
     double d_price; // 现在的价格
 
-    ofstream outFile;   // 创建对象输出
+    ofstream outFile;   // 2.创建对象,输出流
+    outFile.open("carinfo.txt");     // 3.关联一个文件
 
     cout << "Enter the make and model of automobile: ";
     cin.get(automobile, 50);
@@ -25,11 +27,23 @@ int main()
     d_price = a_price * 0.913;
 
     // 内容进行显示
+    cout << fixed;  // 显示不要以科学技术法的显示，以小数点来显示
+    cout.precision(2);  // 输出到小数点后2位
+    cout.setf(ios_base::showpoint); // 零也输出,不省略
     cout << "Make and model: " << automobile << endl;
     cout << "Year: " << year << endl;
     cout << "Was asking $" << a_price << endl;
     cout << "Now asking $" << d_price << endl;
 
+    // 使用outFile替代输出做同样的输出过程
+    outFile << fixed;  // 显示不要以科学技术法的显示，以小数点来显示
+    outFile.precision(2);  // 输出到小数点后2位
+    outFile.setf(ios_base::showpoint); // 零也输出,不省略
+    outFile << "Make and model: " << automobile << endl;
+    outFile << "Year: " << year << endl;
+    outFile << "Was asking $" << a_price << endl;
+    outFile << "Now asking $" << d_price << endl;
 
+    outFile.close();    // 关闭打开的文件
     return 0;
 }
---
+++ c++_primer_plus/ch06/16_sumafile.cpp
@@ -0,0 +1,56 @@
+//
+// Created by 84019 on 2022/9/12.
+//
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+const int SIZE = 60;
+
+int main()
+{
+    char filename[SIZE];
+    ifstream inFile;
+
+    cout << "Enter name of data file: ";
+    cin.getline(filename, SIZE);
+    inFile.open(filename);
+    if(!(inFile.is_open()))   // 打开失败
+    {
+        cout << "Could not open the file " << filename << endl;
+        cout << "Program terminating.";
+        exit(EXIT_FAILURE);
+    }
+    cout << "Success open the txt file.";
+
+    double value;
+    double sum = 0.0;
+    int counts = 0;
+
+    inFile >> value;
+    while (inFile.good())
+    {
+        ++counts;
+        sum += value;
+        inFile >> value;
+    }
+    if (inFile.eof())
+        cout << "End of file reached.\n";
+    else if (inFile.fail())
+        cout << "Input terminated by data mismatch.\n";
+    else
+        cout << "Input terminated for unknown reason.\n";
+
+    if (counts == 0)
+        cout << "No data processed.\n";
+    else
+    {
+        cout << "Items read: " << counts << endl;
+        cout << "Sum: " << sum << endl;
+        cout << "Average: " << sum/counts << endl;
+    }
+    inFile.close();
+    return 0;
+}
