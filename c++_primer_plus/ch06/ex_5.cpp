b1325d90985645f506001cb7f2ed04b7ee147f73
眰恦 <840194634@qq.com>
2022-09-15 14:14:38 +0800

    学习

---
+++ c++_primer_plus/ch06/ex_5.cpp
@@ -0,0 +1,40 @@
+//
+// Created by 84019 on 2022/9/15.
+//
+
+#include <iostream>
+
+using namespace std;
+void tax_calculate(long input);
+
+int main(){
+    long input;
+    cout << "Please enter your income:";
+    while (cin >> input && input > 0)
+    {
+        tax_calculate(input);
+        cout << "Please enter your income:";
+    }
+
+    return 0;
+}
+
+void tax_calculate(long input){
+    double tax=0.00;
+    if (input<=5000){
+        cout << "You don`t need pay any tax" << endl;
+    } else if (input >= 5001 && input <= 15000){
+        tax = (input-5000)*0.10;
+        cout << "You need pay " << tax <<" tvarp." << endl;
+    } else if (input >= 15001 && input <= 35000){
+        tax = (10000*0.10) + ((input-15000)*0.15);
+        cout << "You need pay " << tax <<" tvarp." << endl;
+    } else if (input >= 35001 ){
+        tax = (10000*0.10) + (20000*0.15) + ((input-35000)*0.20);
+        cout << fixed;      // 不要以科学技术法显示
+        cout.precision(2);      // 输出小数点后两位
+        cout.setf(ios_base::showpoint);    // 零也输出
+        cout << "You need pay " << tax <<" tvarp." << endl;
+    }
+
+}
