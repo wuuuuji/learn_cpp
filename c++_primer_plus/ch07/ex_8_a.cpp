4d309175984ddb53d28fe893228507a670e7aba9
眰恦 <840194634@qq.com>
2022-10-09 18:45:42 +0800

    学习

---
+++ c++_primer_plus/ch07/ex_8_a.cpp
@@ -0,0 +1,42 @@
+//
+// Created by 84019 on 2022/10/9.
+//
+
+#include <iostream>
+#include<string>
+
+using namespace std;
+const int Seasons = 4;
+const char * Snames[] = {
+        "spring","Summer","Fall","Winter"
+};
+void fill(double arr[], int size);
+void show(const double arr[], int size);
+
+
+int main(void)
+{
+    double expenses[Seasons];
+    fill(expenses, Seasons);
+    show(expenses, Seasons);
+    return 0;
+}
+
+void fill(double arr[], int size)
+{
+    for (int i = 0; i < size; i++) {
+        cout << "Enter " << Snames[i] << " expenses: ";
+        cin >> arr[i];
+    }
+}
+
+void show(const double arr[], int size)
+{
+    double total = 0.0;
+    cout << "\nEXPENSES\n";
+    for (int i = 0; i < size; i++) {
+        cout << Snames[i] << ": $" << arr[i] << endl;
+        total += arr[i];
+    }
+    cout << "Total Expenses: $" << total << endl;
+}
